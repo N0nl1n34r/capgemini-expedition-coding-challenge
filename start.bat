90fde3348a497bfe39a9b15ae3b7ffaf113d2867
Julian K <koch@in.tum.de>
2021-07-29 19:06:14 +0200

    start scripts

---
+++ start.bat
@@ -0,0 +1,13 @@
+rem Path to python3
+set dataaggr_python="\path\to\python3"
+
+rem Path to the rules file
+set dataaggr_rules="recommendation-system\rules\capgemini_coding_challenge_example_rules"
+
+rem Pi communications port
+set dataaggr_port=8080
+
+rem Refresh period in milliseconds
+set dataaggr_period=1000
+
+java -jar serv-data-aggr.jar
--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Path to python3
-dataaggr_python="/path/to/python3"
+export dataaggr_python="/path/to/python3"
 
 # Path to the rules file
-dataaggr_rules="recommendation-system/rules/capgemini_coding_challenge_example_rules"
+export dataaggr_rules="recommendation-system/rules/capgemini_coding_challenge_example_rules"
 
 # Pi communications port
-dataaggr_port=8080
+export dataaggr_port=8080
 
 # Refresh period in milliseconds
-dataaggr_period=1000
+export dataaggr_period=1000
 
 java -jar serv-data-aggr.jar
